bacc65e112ca010c4ab249e3e16ace6ebc094d58
Orquesta Sinfónica W.T <orquestasinfonicaw.t.@192.168.1.128>
2026-03-16 23:23:02 -0300

    Revert Hostinger changes and stick to Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,7 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       host: '0.0.0.0',
     },
-    base: './',
-    build: {
-      emptyOutDir: true,
-      assetsDir: 'assets',
-    },
+    base: '/',
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
